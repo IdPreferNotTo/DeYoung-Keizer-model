2c12b24ffcbe5716d1a9c1e302e549d431edfc48
SidSloth <ramlowlukas@gmail.com>
2020-07-13 19:44:25 +0200

    added build folder to repo

---
+++ cmake-build-debug/CMakeFiles/dyk_calcium_phd.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dyk_calcium_phd.dir/main.cpp.o"
+  "dyk_calcium_phd"
+  "dyk_calcium_phd.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dyk_calcium_phd.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
